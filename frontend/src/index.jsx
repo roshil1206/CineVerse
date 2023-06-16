b34cf16ec8c69fcb5c0958ec5675ac9507d5024a
Roshil Amitbhai Ka Patel <rs622844@dal.ca>
2023-06-15 21:38:25 -0300

    Refactored the project

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
-import store from "./store";
+// import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      {/* <Provider store={store}> */}
+      <App />
+      {/* </Provider> */}
     </BrowserRouter>
   </React.StrictMode>
 );
