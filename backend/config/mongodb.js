cb8115bd56d1707d93eee87c0e50ab4c26bc4f0c
Roshil Ka Patel <roshilkapatel@t83fd.wpa.dal.ca>
2023-07-17 14:47:16 -0300

    Added single api

---
+++ backend/config/mongodb.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const options = {
+  autoIndex: true,
+};
+
+const username = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
+
+const uri = `mongodb+srv://${username}:${password}@mycluster.b2n6lft.mongodb.net/?retryWrites=true&w=majority`;
+
+async function connect() {
+  try {
+    await mongoose.connect(uri, options);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.log("Unable to connect to MongoDB", err);
+  }
+}
+
+module.exports = {
+  connect,
+};
---
+++ backend/routes/admin/foodAndBeverages.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const FoodAndBeveragesModel = require("../../models/FoodAndBevereges");
+const response = require("../../utils/response");
+
+router.get("/getItems", async (req, res) => {
+  const data = await FoodAndBeveragesModel.find();
+  response(res, 200, true, data);
+});
+
+module.exports = router;
